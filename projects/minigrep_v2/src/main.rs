90f6ac4d4a2e782c0b8f5efb6223921f458585db
VLN37 <jfpr1237@gmail.com>
2023-04-09 22:38:58 -0300

    refactor minigrep V2

--- projects/minigrep_v2/src/main.rs
+++ projects/minigrep_v2/src/main.rs
@@ -17,14 +17,7 @@ struct Cli {
   path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-  env_logger::init();
-  let args = Cli::parse();
-
-  log::info!("Minigrep V2 has started");
-  let file = File::open(&args.path)
-  .with_context(|| format!("could not read file {:#?}", args.path))?;
-
+fn find_matches(file: &File, args: &Cli) {
   let content = BufReader::new(file);
   for result in content.lines() {
     let line = result.unwrap_or_else(|error| panic!("{:?}", error));
@@ -32,6 +25,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
       println!("{}", line)
     }
   }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  env_logger::init();
+  let args = Cli::parse();
+
+  log::info!("Minigrep V2 has started");
+
+  let file = File::open(&args.path)
+    .with_context(|| format!("could not read file {:#?}", args.path))?;
+  find_matches(&file, &args);
+
   log::info!("Minigrep V2 operation succesful");
   Ok(())
 }
